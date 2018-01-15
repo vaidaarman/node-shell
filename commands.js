d98cb71f324688d78e890c84dea61ca55f7ee736
Vaida Armanaviciute <vaidaa@Vaidas-MacBook-Pro.local>
2018-01-15 13:42:20 -0600

    update

--- commands.js
+++ commands.js
@@ -1,34 +1,32 @@
-let fileSystem = require('fs')
+let fileSystem = require("fs");
 
 module.exports = {
   pwd: function() {
     process.stdout.write(process.cwd());
-    process.stdout.write('\nprompt > ');
+    process.stdout.write("\nprompt > ");
   },
   ls: function() {
-    return fileSystem.readdir('.', function(err, files) {
-        if (err) throw err;
-        files.forEach(function(file) {
-          process.stdout.write(file.toString() + "\n");
-        })
-        process.stdout.write("prompt > ");
-      });   
+    return fileSystem.readdir(".", function(err, files) {
+      if (err) throw err;
+      files.forEach(function(file) {
+        process.stdout.write(file.toString() + "\n");
+      });
+      process.stdout.write("prompt > ");
+    });
   },
-  echo: function (text){
+  echo: function(text) {
     console.log(text);
     process.stdout.write("prompt > ");
-},
-  cat: function (file) {
-      console.log(fileSystem.readFile(file, 'utf8')) 
-    //   function(err, data) {  
-    //     if (err) throw err;
-    //     console.log(data);
-        process.stdout.write("prompt > "); 
-    // }
-// )
-// );
   },
-  date: function (){
+  cat: function(file) {
+    console.log(fileSystem.readFile(file, "utf8", function(err, data) {
+        if (err) throw err;
+        console.log(data);
+        process.stdout.write("prompt > ");
+      })
+    );
+  },
+  date: function() {
     var date = new Date();
     process.stdout.write(date.toString());
     process.stdout.write("\nprompt > ");
